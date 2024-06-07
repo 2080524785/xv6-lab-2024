41496417db5b0fc27c96e303238856a0463ace3b
2080524785 <2080524785@qq.com>
2024-06-07 16:52:37 +0800

    lab2:tarce finish, and study how to use gdb to debug

--- kernel/proc.c
+++ kernel/proc.c
@@ -289,6 +289,8 @@ fork(void)
       np->ofile[i] = filedup(p->ofile[i]);
   np->cwd = idup(p->cwd);
 
+  np->mask = p->mask;
+
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
