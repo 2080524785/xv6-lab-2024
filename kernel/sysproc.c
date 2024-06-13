bba1b53ecaa3badde3f7e7d3fb28f572203501e8
2080524785 <2080524785@qq.com>
2024-06-13 14:48:23 +0800

    lab2:sysinfo

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,7 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
-
+#include "sysinfo.h"
 uint64
 sys_exit(void)
 {
@@ -103,4 +103,19 @@ sys_trace(void){
   myproc()->mask = n;
   return 0;
   
+}
+
+uint64
+sys_sysinfo(void){
+  uint64 addr;
+  if(argaddr(0, &addr)<0){
+    return -1;
+  }
+  struct sysinfo info;
+  info.freemem = get_kfree();
+  info.nproc = get_pfree();
+
+  if(copyout(myproc()->pagetable, addr, (char *)&info, sizeof(info))<0) return -1;
+  return 0;
+
 }
